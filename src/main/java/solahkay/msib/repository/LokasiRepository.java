7f0f483feb652db71ecbea9be586f39be4472744
Solahudin Al Malibari <solahprogramming@gmail.com>
2024-08-18 06:02:52 +0700

    add: entity repositories

---
+++ src/main/java/solahkay/msib/repository/LokasiRepository.java
@@ -0,0 +1,7 @@
+package solahkay.msib.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import solahkay.msib.entity.Lokasi;
+
+public interface LokasiRepository extends JpaRepository<Lokasi, Integer> {
+}
---
+++ src/main/java/solahkay/msib/repository/ProyekRepository.java
@@ -0,0 +1,7 @@
+package solahkay.msib.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import solahkay.msib.entity.Proyek;
+
+public interface ProyekRepository extends JpaRepository<Proyek, Integer> {
+}
