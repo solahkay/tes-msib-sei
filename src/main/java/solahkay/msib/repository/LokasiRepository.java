3fe376599fd5e7a39437011941ae9ee943c30e15
Solahudin Al Malibari <solahprogramming@gmail.com>
2024-08-18 07:02:15 +0700

    update: LokasiRepository

--- src/main/java/solahkay/msib/repository/LokasiRepository.java
+++ src/main/java/solahkay/msib/repository/LokasiRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import solahkay.msib.entity.Lokasi;
 
 public interface LokasiRepository extends JpaRepository<Lokasi, Integer> {
+
+    boolean existsByNamaLokasi(String namaLokasi);
+
 }
